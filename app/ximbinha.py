4d49e13cc35cf6db3c006e5d66e045f29056ca98
fernandaformiga <ferformigas@gmail.com>
2021-04-12 18:22:46 -0300

    Insert solution file and comment ximbinha.py

--- app/ximbinha.py
+++ app/ximbinha.py
@@ -1,24 +1,30 @@
-# import os
-# import sys
+import os
+import sys
+# sys.path.append("..")
+
+# sys.path.insert(0,
+#     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 # PROJECT_ROOT = os.path.dirname(__file__)
 # sys.path.insert(0, os.path.join(PROJECT_ROOT, "CRUD-LOJA"))
 
 # from setuptools import setup, find_packages
 # from package import module 
-from ..src.category.dao.category_dao import *
-from ..src.product.dao.product_dao import ProductDAO
-from ..src.product_category.dao.product_category_dao import ProductCategoryDao
-
+from src.category.dao.category_dao import CategoryDAO
+from src.product.dao.product_dao import ProductDAO
+from src.product_category.dao.product_category_dao import ProductCategoryDao
 
 def controller():
-    category = CategoryDAO()
-    product = ProductDAO()
-    product_category = ProductCategoryDao()
-
+    # category = CategoryDAO()
+    # product = ProductDAO()
+    # product_category = ProductCategoryDao()
 
     category.create_table_category()
     product.create_table_product()
     product_category.create_table_product_category()
+
+    
 
 if __name__ == '__main__':
     controller()
