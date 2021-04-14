df50a39f1baaa25584cc35b5b3897aa1eea09802
danylagacione <danielycrys24@gmail.com>
2021-04-14 14:54:40 -0300

    alterado metodos read all-read by id e model

--- src/product/dao/product_dao.py
+++ src/product/dao/product_dao.py
@@ -13,25 +13,46 @@ class ProductDAO(Dao):
             );
         """)
 
-    def insert_data_product(self, product:Product):
-        self.cursor.execute("""
+    def create(self, product:Product):
+        sql = ("""
         INSERT INTO product (name, description, price) VALUES (?, ?, ?)
-        """, (product.name, product.description, product.price))
-        self.commit()
-        id = self.cursor.lastrowid
-        print("oi", id)
-        return id
+        """)
+        parameters = (product.name, product.description, product.price)
+        return self.insert_data(sql, parameters)
 
-    def select_all_data_product(self):
-        self.cursor.execute("""
+    # def select_all_data_product(self):
+    #     self.cursor.execute("""
+    #     SELECT product.name, product.description, product.price, group_concat(category.name)
+    #     FROM product JOIN product_category ON product_id = product.id JOIN category ON category_id = category.id
+    #     GROUP BY product.name, product.description, product.price
+    #     """)
+
+    #     for product in self.cursor.fetchall():
+    #         print(product)
+
+    def read_all_products_with_category(self):
+        sql = ("""
         SELECT product.name, product.description, product.price, group_concat(category.name)
         FROM product JOIN product_category ON product_id = product.id JOIN category ON category_id = category.id
-        GROUP BY product.name, product.description, product.price
-        """)
+        GROUP BY product.name, product.description, product.price """)
+        list_products_with_category = []
+        result = self.execute_query_select(sql)
+        for item in result:
+            products = Product(item[1], item[2],item[3], item[4], item[0])
+            list_products_with_category.append(products)
+        return list_products_with_category
 
-        for product in self.cursor.fetchall():
-            print(product)
-        
+    def read_by_id(self, id: int) -> Product:
+        sql = ("""
+        SELECT * FROM product WHERE id = ?
+        """)
+        parameter = id
+        result = self.execute_query_select(sql, parameter)
+        item =  result[0]
+        product = Product(item[1], item[2],item[3], item[4], item[0])
+        return product
+    
+         
     def select_data_product(self, product:Product):
         print(self.cursor.execute("""
         SELECT * FROM product WHERE id = ?
