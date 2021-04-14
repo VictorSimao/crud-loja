9b0bb4ca9c5863a542efbd4881aa5fcfe2a94e5c
TarcisioCardosoJunior <tarcisio.cardoso@olist.com>
2021-04-14 17:02:55 -0300

    refactor menu script

--- main.py
+++ main.py
@@ -23,27 +23,78 @@ class Main:
         self.product_category_dao.create_table_product_category()
 
         while True:
-            self.get_user_input()
+            self.main_menu()
+
+    def main_menu(self):
+        print("""
+            Seja Bem Vindo ao menu de registro produtos !!!
 
 
+            Selecione a opção desejada:
 
-    def get_user_input(self):
+            1 - Menu de Produtos
+            2 - Menu de Categorias
+            3 - Sair 
+        """)
+        choice = input()
+        if choice == '1':
+            self.menu_product()
+        elif choice == '2':
+            self.menu_category()
+        self.exit_menu(choice=choice, callback=self.main_menu)
+
+    def menu_category(self):
+        print("""O que você deseja fazer? Selecione uma das opções abaixo:
+        1. Listar as categorias
+        2. Cadastrar uma categoria
+        3. Sair""")
+        choice = input()
+        self.main_menu_category(choice=choice)
+        
+
+
+    def menu_product(self):
         print("""O que você deseja fazer? Selecione uma das opções abaixo:
         1. Listar os produtos
         2. Cadastrar um produto
-        3. Listar as categorias
-        4. Cadastrar uma categoria
-        5. Sair""")
+        3. Sair""")
         choice = input()
-        self.get_choice(choice)
+        self.main_menu_product(choice=choice)
 
     def show_categories(self) -> str:
         categories = self.category_dao.read_all()
         return [print(f"{cat.id} - {cat.name} - {cat.description}") for cat in categories] 
     
 
-    def get_choice(self, choice):
-        condition = True
+    def menu_register_product(self):
+        selected_categories = []
+        print("Selecione uma das categorias abaixo:")
+        self.show_categories()
+        selected_categories.append(int(input()))
+        option = input("Você deseja cadastrar mais uma categoria? (s/N): ")
+        if option == "s":
+            self.menu_register_product()
+        return selected_categories
+            
+    def main_menu_category(self,choice):
+        if choice == "1":
+            self.show_categories()
+        elif choice == "2":
+            category_name = input("Escreva o nome da categoria:")
+            category_description = input("Escreva a descrição da categoria:")
+            category = Category(category_name, category_description)
+            self.category_id = self.category_dao.create(category)
+        self.exit_menu(choice=choice, callback=self.menu_category)
+
+    def exit_menu(self,choice, callback):
+        if choice == "3":
+            sys.exit(1)
+        elif int(choice) > 3 or type(int(choice)) != int:
+            print("Opção inválida")
+            callback()
+
+
+    def main_menu_product(self, choice):
         if choice == "1":
             products = self.product_dao.read_all()
             [print(f"{product.id} - {product.name} - {product.description} - {product.price} - {product.categories}") for product in products]
@@ -51,33 +102,12 @@ class Main:
             product_name = input("Escreva o nome do produto:")
             product_description = input("Escreva a descrição do produto:")
             product_price = input("Escreva o preço do produto:")
-            selected_categories = []
-            while condition:
-                print("Selecione uma das categorias abaixo:")
-                self.show_categories()
-                selected_categories.append(int(input()))
-                option = input("Você deseja cadastrar mais uma categoria? (s/N): ")
-                if option == "s":
-                    continue
-                else:
-                    condition = False
+            selected_categories = self.menu_register_product()
             categories = self.category_dao.read_by_id(selected_categories)
             product = Product(product_name, product_description, product_price, categories)
             self.product_id = self.product_dao.create(product)
             for selected_category in selected_categories:
                 self.product_category_dao.create(self.product_id, selected_category)
-        elif choice == "3":
-            self.show_categories()
-        elif choice == "4":
-            category_name = input("Escreva o nome da categoria:")
-            category_description = input("Escreva a descrição da categoria:")
-            category = Category(category_name, category_description)
-            self.category_id = self.category_dao.create(category)
-        elif choice == "5":
-            sys.exit(1)
-        else:
-            print("Opção inválida")
-            self.get_user_input()
-
+        self.exit_menu(choice, callback=self.menu_product)
 
 Main()
