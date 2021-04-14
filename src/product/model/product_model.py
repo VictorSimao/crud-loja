1d741d4f5847e6eecc7c20acef5cbc3ffae9655f
Felipe Savaris <fesavaris@gmail.com>
2021-04-14 15:45:07 -0300

    alteraçoes na product_dao para atribuir a alteracao na camada de persistencia

--- src/product/model/product_model.py
+++ src/product/model/product_model.py
@@ -1,7 +1,8 @@
 class Product:
-    def __init__(self, name, description, price, categories):
+    def __init__(self, name, description, price, categories, id=None):
         self.name = name
         self.description = description
         self.price = price
         self.categories = categories
+        self.id = id
         
