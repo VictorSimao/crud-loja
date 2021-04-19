4b0514b3c0381fe0fa66ec58470c4f205e899289
Felipe Savaris <fesavaris@gmail.com>
2021-04-19 14:10:13 -0300

    add função de exclusão ao projeto

--- src/view/console/view_product.py
+++ src/view/console/view_product.py
@@ -23,5 +23,6 @@ class ViewProduct:
         self.controller.update()
 
     def delete(self):
-        #inputs
-        self.controller.delete()
+        self.read()
+        product_id = int(input("escolha uma produto que deseja deletar: "))
+        self.controller.delete(product_id)
