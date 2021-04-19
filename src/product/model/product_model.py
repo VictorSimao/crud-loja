64748d80d11b68057c17c511f77f46f4252f4b41
Victor Simao <victor.simao@olist.com>
2021-04-19 14:03:05 -0300

    deletando pastas vazias

--- src/product/model/product_model.py
+++
@@ -1,7 +0,0 @@
-class Product:
-    def __init__(self, name, description, price, id=None, categories=None):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.categories = categories
-        self.id = id
