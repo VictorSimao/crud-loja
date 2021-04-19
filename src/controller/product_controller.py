e52ed414319594132b589f7be7574faa5ee9e9a5
Felipe Savaris <fesavaris@gmail.com>
2021-04-19 13:41:58 -0300

    alterada a lógica para a controller de produtos.

--- src/controller/product_controller.py
+++ src/controller/product_controller.py
@@ -7,17 +7,27 @@ class ProductController:
         self.dao = ProductDAO()
 
     def create(self):
-        model = Product()
-        self.dao.create_product(model)
+        # model = Product()
+        # self.dao.create_product(model)
+        product = Product(name, description, price)
+        product_id = self.dao.create(product)
 
     def read(self):
-        list_models = self.dao.read_all_products()
-        return list_models
+        # list_models = self.dao.read_all_products()
+        # return list_models
+        products = self.dao.read_all()
+        return products
 
-    def update(self):
-        model = Product()
-        self.dao.update_product(model)
+    def read_by_id(self, product_id):
+        Product = self.dao.read_by_id(product_id)
+        return Product
 
-    def delete(self):
-        id = 0
-        self.dao.delete_product(id)
+    def update(self, product):
+        # model = Product()
+        # self.dao.update_product(model)
+        self.dao.update(product)
+
+    def delete(self, product_id):
+        # id = 0
+        # self.dao.delete_product(id)
+        self.dao.delete(product_id)
