5385808b4bbbb4ed4776e89286bce27b537b2493
danilovbarbosa <danilo.v.barbosa@gmail.com>
2021-04-19 15:00:14 -0300

    feat(view_product/ViewProduct): adicionado feat para print de produtos.

--- src/view/console/view_product.py
+++ src/view/console/view_product.py
@@ -11,8 +11,16 @@ class ViewProduct:
         self.controller.create()
 
     def read(self):
-        self.controller.read()
-        #return list
+        products = self.controller.read()
+        
+        for prod in products:
+            products_categories = self.product_category_dao.read_categories_by_product_id(prod.id,)
+            list_categories = []
+            for prod_cat in products_categories:
+                category = self.category_dao.read_by_id(prod_cat.category_id)
+                list_categories.append(category)
+            prod.categories = list_categories
+            print(prod)
 
     def update(self):
         #inputs
