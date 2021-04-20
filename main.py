e5524a0d0d1d6044d238286ede33674fe8510524
Maykon D. Graneman <maykon.granemann@olist.com>
2021-04-20 14:31:05 -0300

    View  Web

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from src.view.console.view_menu import ViewMenu
+from src.view.web.start import app
 
-console = ViewMenu()
-console.execute()
+
+#console = ViewMenu()
+#console.execute()
+
+app.run(debug=True)
---
+++ src/view/web/start.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect
+
+from src.controller.category_controller import CategoryController
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/category')
+def category():
+    controller = CategoryController()
+    data = controller.read()
+    return render_template('category.html', title="Category", data=data)
+
+@app.route('/category/form')
+def category_create():
+    category_id = request.args.get('id')
+    if category_id:
+        controller = CategoryController()
+        data = controller.read_by_id(category_id)
+        return render_template('category_form.html', title="Category Update", data=data)
+    return render_template('category_form.html', title="Category Create")
+
+@app.route('/category/save')
+def category_save():
+    category_id = request.args.get('id')
+    name = request.args.get('name')
+    description = request.args.get('description')
+
+    controller = CategoryController()
+    if category_id:
+        controller.update(category_id, name, description)
+    else:
+        controller.create(name, description)
+
+    return redirect('/category')
+
+@app.route('/category/delete')
+def category_delete():
+    category_id = request.args.get('id')
+    controller = CategoryController()
+    controller.delete(category_id)
+
+    return redirect('/category')
+
