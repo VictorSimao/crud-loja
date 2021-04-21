d5e3764f8d64dbe222cddbee7ec016d1c0295d9c
danylagacione <danielycrys24@gmail.com>
2021-04-21 15:20:48 -0300

    falta ajustes do html

--- src/views/web/start.py
+++ src/views/web/start.py
@@ -1,23 +1,41 @@
 from flask import Flask, render_template, request, redirect
 
 from src.controllers.category_controller import CategoryController
-
+from src.controllers.product_controller import ProductController
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/product')
 def product():
-    return render_template('product.html')
+    product_controller = ProductController()
+    data = product_controller.read()
+    return render_template('product.html', title="Product", data=data)
+
 
 @app.route('/category')
 def category():
     controller = CategoryController()
     data = controller.read()
     return render_template('category.html', title="Category", data=data)
+
+
+@app.route('/product/form')
+def product_create():
+    product_id = request.args.get('id')
+    controller = ProductController()
+    category_controller = CategoryController()
+    categories = category_controller.read()
+    if product_id:
+        data = controller.read_by_id(product_id)
+        return render_template('product_form.html', title="Product Update", data=data, categories=categories)
+    return render_template('product_form.html', title="Product Create", categories=categories)
+
 
 @app.route('/category/form')
 def category_create():
@@ -28,27 +46,56 @@ def category_create():
         return render_template('category_form.html', title="Category Update", data=data)
     return render_template('category_form.html', title="Category Create")
 
+
 @app.route('/category/save')
 def category_save():
-    category_id = request.args.get('id')
-    name = request.args.get('name')
-    description = request.args.get('description')
-
+    category = {
+        'id': request.args.get('id'),
+        'name': request.args.get('name'),
+        'description': request.args.get('description'),
+    }
     controller = CategoryController()
-    if category_id:
-        controller.update(category_id, name, description)
+    if category['id']:
+        controller.update(category)
     else:
-        controller.create(name, description)
-
+        controller.create(category)
     return redirect('/category')
+
+
+@app.route('/product/save')
+def product_save():
+    product_dict = request.args.to_dict(flat=False)
+    categories = product_dict.get("categories", None)
+    product = {
+        'id': request.args.get('id'),
+        'name': request.args.get('name'),
+        'description': request.args.get('description'),
+        'price': request.args.get('price'),
+        'categories': categories
+        }
+    product_controller = ProductController()
+    if product['id']:
+        product_controller.update(product)
+    else:
+        product_controller.create(product)
+    return redirect('/product')    
+
 
 @app.route('/category/delete')
 def category_delete():
     category_id = request.args.get('id')
     controller = CategoryController()
     controller.delete(category_id)
-
     return redirect('/category')
+
+
+@app.route('/product/delete')
+def product_delete():
+    product_id = request.args.get('id')
+    product_controller = ProductController()
+    product_controller.delete(product_id)
+
+    return redirect('/product')
 
 
 if __name__ == "__main__":
